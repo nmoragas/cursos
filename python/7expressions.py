b6d7d2c3d611499202a9b6a7251a7c31f7ead89b
nmoragas <n.moragas90@gmail.com>
2025-06-11 07:15:34 +0200

    Update 7expressions.py

--- python/7expressions.py
+++ python/7expressions.py
@@ -30,3 +30,47 @@ if __name__ == "__main__":
     main()
 
 #####################################################################################################################################################################
+
+
+
+# EXRECICI 2
+
+'''
+inputs: 
+http://youtube.com/embed/xvFZjo5PgG0
+https://youtube.com/embed/xvFZjo5PgG0
+https://www.youtube.com/embed/xvFZjo5PgG0
+
+
+"harvard\.edu", utilitzeu r"harvard\.edu". -> per evitar problemes amb \
+
+# exemple input: <iframe src="http://www.youtube.com/embed/xvFZjo5PgG0"></iframe>
+#exeple resultat: https://youtu.be/xvFZjo5PgG0
+'''
+
+import re
+import sys
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    
+
+    
+    pattern = r'src="https?://(www\.)?youtube\.com/embed/([a-zA-Z0-9_-]+)"'
+    match = re.search(pattern, s)
+    
+
+    if match:
+        video_id = match.group(2)  # segon grup del match segons ()
+        return f"https://youtu.be/{video_id}"
+    else:
+        return "False"
+
+
+if __name__ == "__main__":
+    main()
+    
