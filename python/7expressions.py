cb7078ea28f5b853d12d24c6b1599f1edc0f678a
nmoragas <n.moragas90@gmail.com>
2025-06-19 07:24:45 +0200

    Update 7expressions.py

--- python/7expressions.py
+++ python/7expressions.py
@@ -81,7 +81,33 @@ if __name__ == "__main__":
 ####################3
 
 # EXERCICI 3:
+import re
+import sys
 
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    pattern = r"^(\d+)\.(\d+)\.(\d+)\.(\d+)$"
+    match = re.match(pattern, ip)
+    if match:
+        for part in match.groups():
+            if not 0 <= int(part) <= 255:
+                return False
+        return True
+    return False
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+# test
 
 
 
